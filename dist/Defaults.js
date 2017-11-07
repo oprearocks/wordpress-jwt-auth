3173bf6f012366194f20b5816c6ed0a4bec5fecd
Adrian Oprea <adrian@oprea.rocks>
2017-11-07 18:02:47 +0200

    chore: Resulting JavaScript build files
    Signed-off-by: Adrian Oprea <adrian@oprea.rocks>

---
+++ dist/Defaults.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    JWT_ENDPOINT: '/wp-json/jwt-auth/v1',
+    JWT_ROUTE_VALIDATE: '/validate',
+    JWT_ROUTE_GENERATE: '/token',
+};
+//# sourceMappingURL=Defaults.js.map
