63734c8674647b039b2dd44d95c3c88b3110e092
Adrian Oprea <adrian@oprea.rocks>
2017-11-07 18:01:56 +0200

    feat: Add default options module
    Signed-off-by: Adrian Oprea <adrian@oprea.rocks>

---
+++ lib/Defaults.ts
@@ -0,0 +1,11 @@
+export default {
+    JWT_ENDPOINT: '/wp-json/jwt-auth/v1',
+    JWT_ROUTE_VALIDATE: '/validate',
+    JWT_ROUTE_GENERATE: '/token',
+};
+
+export interface OPTIONS {
+    JWT_ENDPOINT: string;
+    JWT_ROUTE_VALIDATE: string;
+    JWT_ROUTE_GENERATE: string;
+}
