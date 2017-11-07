6501d60d33106dc342de278a47e92f89476f71d3
Adrian Oprea <adrian@oprea.rocks>
2017-11-07 18:02:28 +0200

    chore: Automatically updated typings
    Signed-off-by: Adrian Oprea <adrian@oprea.rocks>

---
+++ dist/Defaults.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    JWT_ENDPOINT: string;
+    JWT_ROUTE_VALIDATE: string;
+    JWT_ROUTE_GENERATE: string;
+};
+export default _default;
+export interface OPTIONS {
+    JWT_ENDPOINT: string;
+    JWT_ROUTE_VALIDATE: string;
+    JWT_ROUTE_GENERATE: string;
+}
